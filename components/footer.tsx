afe38c50da44e827c83c139aabb41434d8fb4bb3
Ohata Kenji <11045367+OhataKenji@users.noreply.github.com>
2022-03-14 00:43:43 +0900

    fix name

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Ohata Kenji(Suzuki Ken) Portfolio
+            Ohata Kenji (Suzuki Ken) Portfolio
           </h3>
         </div>
       </Container>

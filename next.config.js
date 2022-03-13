65ea6d0e9e518f0255463beed73aa602dc8c7faa
Ohata Kenji <11045367+OhataKenji@users.noreply.github.com>
2022-03-14 00:23:41 +0900

    fix: asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const isProd = process.env.NODE_ENV === "production"
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    assetPrefix: isProd ? "/portfolio" : "",
     basePath: process.env.GITHUB_ACTIONS ? "/portfolio" : "",
     trailingSlash: true,
 }

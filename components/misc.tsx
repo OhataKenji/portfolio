5e36af2ac8d93a71b00af28ec4798a14c4e18f37
Ohata Kenji <11045367+OhataKenji@users.noreply.github.com>
2022-03-14 20:54:37 +0900

    fix: misc description font size

--- components/misc.tsx
+++ components/misc.tsx
@@ -6,15 +6,15 @@ import MiscItem from './misc-item'
 const items = [
     {
         'title': 'MITの深層学習の講義レポジトリにある正規表現の修正プルリクエスト',
-        'child': (<span>Juliaで講義中のコードを再実装しようとしていた際に発見した。<Link href={"https://github.com/aamini/introtodeeplearning/pull/65"}><a className="hover:underline">(リンク)</a></Link></span>)
+        'child': (<span className="text-lg leading-relaxed mb-4">Juliaで講義中のコードを再実装しようとしていた際に発見した。<Link href={"https://github.com/aamini/introtodeeplearning/pull/65"}><a className="hover:underline">(リンク)</a></Link></span>)
     },
     {
         'title': 'サバイバルTypeScriptの誤字修正プルリクエスト',
-        'child': (<span>TypeScript勉強中に発見した。<Link href={'https://github.com/yytypescript/book/pull/13'}><a className="hover:underline">(リンク)</a></Link></span>)
+        'child': (<span className="text-lg leading-relaxed mb-4">TypeScript勉強中に発見した。<Link href={'https://github.com/yytypescript/book/pull/13'}><a className="hover:underline">(リンク)</a></Link></span>)
     },
     {
         'title': 'ECサイトの脆弱性報告',
-        'child': (<span>地元の模型店が利用しているレンタルサーバーが提供するEC機能に商品価格を変更可能なバグがあることを発見し、IPA報告して受理された。</span>)
+        'child': (<span className="text-lg leading-relaxed mb-4">地元の模型店が利用しているレンタルサーバーが提供するEC機能に商品価格を変更可能なバグがあることを発見し、IPA報告して受理された。</span>)
     },
 ]
 
